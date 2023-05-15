f97543b8932354e8cdde4a003d6ec4769bbb87c3
thysania <133405532+thysania@users.noreply.github.com>
2023-05-15 12:12:45 +0100

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -37,7 +37,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" };
+static const char *tags[] = { "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
