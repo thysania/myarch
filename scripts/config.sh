908065cf6bbf082c735f3a61e7b6a9fed8ff02a3
thysania <133405532+thysania@users.noreply.github.com>
2023-05-27 15:53:06 +0100

    Update and rename config to config.sh

---
+++ scripts/config.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+echo "                       _       ______                   "
+echo "     /\               | |     |  ____|                  "
+echo "    /  \    _ __  ___ | |__   | |__    _ __  ___  _ __  "
+echo "   / /\ \  | '__|/ __|| '_ \  |  __|  | '__|/ _ \| '_ \ "
+echo "  / ____ \ | |  | (__ | | | | | |____ | |  |  __/| | | |"
+echo " /_/    \_\|_|   \___||_| |_| |______||_|   \___||_| |_|"
+echo "--------------------------------------------------------"
+
+echo
+echo "  === CONFIGURING THE SYSTEM! ==="
+echo
+
+# installing dwm
+echo "  --> INSTALLING DWM"
+cd ~/myarch/dwm/
+sudo make clean install
+echo""
+
+# installing st terminal
+echo "  --> INSTALLING ST TERMINAL"
+cd ~/myarch/st/
+sudo make clean install
+echo ""
+
+# installing dmenu
+echo "  --> INSTALLING DMENU"
+cd ~/myarch/dmenu
+sudo make clean install
+echo ""
+
+# installing dunst
+echo "  --> INSTALLING DUNST"
+cd ~/myarch/dunst/
+sudo make clean install
+echo ""
+
+# creating user dirs
+echo "  --> CREATING USER DIRS"
+xdg-user-dirs-update
+echo ""
+
+# changing wallpaper
+echo "  --> CHANGING WALLPAPER"
+feh --bg-fill ~/myarch/wallpapers/arch.png
+echo ""
+
+# copying dotfiles
+echo " => Creating config directories"
+if [ -d "~/.config/dunst" ]
+then
+    echo "dunst already exist"
+else
+    mkdir ~/.config/dunst
+fi
+
+echo " => Copying .xinitrc"
+cp .xinitrc ~/.xinitrc
+
+echo " => Copying .bashrc"
+cp .bashrc ~/.bashrc
+
+echo " => Copying .bash_profile"
+cp .bashrc_profile ~/bash_profile
+
+echo " => Copying .zshrc"
+cp .zshrc ~/.zshrc
+
+echo " => Copying dunstrc"
+cp dunstrc ~/.config/dunst/
+
+echo
+echo "Done!"
+echo
