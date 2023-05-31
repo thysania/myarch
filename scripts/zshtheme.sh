fae31ebd346f9ae946e665f41b538a8a539bd648
thysania <133405532+thysania@users.noreply.github.com>
2023-05-31 11:24:16 +0100

    Create zshtheme.sh

---
+++ scripts/zshtheme.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# oh-my-zsh install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# plugins
+# == autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# == syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# == history substring search
+ git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
